d48bf28b06557a9abfe8f6516fe07a1060b7a4c6
LawyerLu <1895@88.com>
2025-02-23 22:20:52 +0800

    修复主题描述

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-/*
+/**
  *
  * 一款简约文艺的文字博客主题：那些物质的东西，都会随着时间慢慢销蚀，而我们写下的文字，最趋近于永恒。希望你在用上这款主题之后，不忘初心，坚持把自己的博客写下去。
+ * 官网：<a href="https://oneblogx.com">oneblogx.com</a>
+ * 文档：<a href="https://oneblogx.com/theme/">oneblogx.com/theme/</a>
  * 
  * @package OneBlog
  * @author 彼岸临窗
  * @version 3.4.3
  * @link https://oneblogx.com
- * @Updated: 2025-02-23
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 if(isMobile()){  //移动端单独编写
